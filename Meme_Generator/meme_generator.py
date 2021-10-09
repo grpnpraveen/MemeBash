1bfe262a905dcfef4c3c5cdc92d64e6b972a53fa
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-10-10 01:09:52 +0530

    Added meme generator in the folder
    main function is at the bottom in the code
    
    read readme and comments in the code @harshithtunuguntla @sishir2001

---
+++ Meme_Generator/meme_generator.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+import sys
+
+
+def make_meme(topString, bottomString, filename):
+
+	img = Image.open(filename)
+	imageSize = img.size
+
+	# find biggest font size that works
+	fontSize = int(imageSize[1]/5)
+	font = ImageFont.truetype("Impact.ttf", fontSize) #"/Library/Fonts/Impact.ttf"
+	topTextSize = font.getsize(topString)
+	bottomTextSize = font.getsize(bottomString)
+	while topTextSize[0] > imageSize[0]-20 or bottomTextSize[0] > imageSize[0]-20:
+		fontSize = fontSize - 1
+		font = ImageFont.truetype("Impact.ttf", fontSize)
+		topTextSize = font.getsize(topString)
+		bottomTextSize = font.getsize(bottomString)
+
+	# find top centered position for top text
+	topTextPositionX = (imageSize[0]/2) - (topTextSize[0]/2)
+	topTextPositionY = 0
+	topTextPosition = (topTextPositionX, topTextPositionY)
+
+	# find bottom centered position for bottom text
+	bottomTextPositionX = (imageSize[0]/2) - (bottomTextSize[0]/2)
+	bottomTextPositionY = imageSize[1] - bottomTextSize[1]-20
+	bottomTextPosition = (bottomTextPositionX, bottomTextPositionY)
+
+	draw = ImageDraw.Draw(img)
+
+	# draw outlines
+	# there may be a better way
+	outlineRange = int(fontSize/15)
+	for x in range(-outlineRange, outlineRange+1):
+		for y in range(-outlineRange, outlineRange+1):
+			draw.text((topTextPosition[0]+x, topTextPosition[1]+y), topString, (0,0,0), font=font)
+			draw.text((bottomTextPosition[0]+x, bottomTextPosition[1]), bottomString, (0,0,0), font=font)
+
+	draw.text(topTextPosition, topString, (255,255,255), font=font)
+	draw.text(bottomTextPosition, bottomString, (255,255,255), font=font)
+
+	img.save("meme_generated.png")
+
+def get_upper(somedata):
+	'''
+	Handle Python 2/3 differences in argv encoding
+	'''
+	result = ''
+	try:
+		result = somedata.decode("utf-8").upper()
+	except:
+		result = somedata.upper()
+	return result
+
+def get_lower(somedata):
+	'''
+	Handle Python 2/3 differences in argv encoding
+	'''
+	result = ''
+	try:
+		result = somedata.decode("utf-8").lower()
+	except:
+		result = somedata.lower()		
+
+	return result
+
+
+
+
+
+#main function here
+
+filename ="successkid.jpg"  #default value ,but this need to be user input image
+
+topString=""  #top string default empty
+bottomString="" #bottom string default empty
+
+#topstring or bottom string based on command these need to be written
+make_meme(topString, bottomString, filename)	
+
